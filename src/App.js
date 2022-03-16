0e9f395dcf1f7e6ad9d5ce98dad7a24f3628da75
kawabata <s1931115@s.konan-u.ac.jp>
2022-03-16 14:55:17 +0900

    inlinestyle and cssmodules

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import {ChildArea} from "./ChildArea";
-import {useState, useCallback, useMemo} from "react";
+// import {ChildArea} from "./ChildArea";
+// import {useState, useCallback, useMemo} from "react";
+//
+import {InLineStyle} from "./components/InLineStyle";
+import {CssModules} from "./components/CssModules";
 
 export const App = () => {
-
-    const [text, setText] = useState('')
-    const [open, setOpen] = useState(false)
-    const [count, setCount] = useState(0)
-
-    const onChangeText = (event) => setText(event.target.value)
-
-    const onClickOpen = () => setOpen(!open)
-
-    const onClickCountUp = () => setCount(count + 1)
-
-    const onClickClose = useCallback(
-        () => setOpen(false),
-        [setOpen])
+//
+//     const [text, setText] = useState('')
+//     const [open, setOpen] = useState(false)
+    // const [count, setCount] = useState(0)
+    //
+    // const onChangeText = (event) => setText(event.target.value)
+    //
+    // const onClickOpen = () => setOpen(!open)
+    //
+    // const onClickCountUp = () => setCount(count + 1)
+    //
+    // const onClickClose = useCallback(
+    //     () => setOpen(false),
+    //     [setOpen])
 
     // const temp = useMemo(() => 1 + 3,[])
 
     return (
         <>
-            <input type="text" value={text} onChange={onChangeText}/>
-            <br/>
-            <br/>
-            <button onClick={onClickOpen}>表示</button>
-            <ChildArea open={open} onClickClose={onClickClose}/>
-            <button onClick={onClickCountUp}>カウントアップ</button>
-            <p>{count}</p>
+            {/*<input type="text" value={text} onChange={onChangeText}/>*/}
+            {/*<br/>*/}
+            {/*<br/>*/}
+            {/*<button onClick={onClickOpen}>表示</button>*/}
+            {/*<ChildArea open={open} onClickClose={onClickClose}/>*/}
+            {/*<button onClick={onClickCountUp}>カウントアップ</button>*/}
+            {/*<p>{count}</p>*/}
+            <InLineStyle/>
+            <CssModules/>
         </>
     )
 }
