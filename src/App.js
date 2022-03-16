b0771cf977d11c23e0f3250c18aa8f39417c51b5
kawabata <s1931115@s.konan-u.ac.jp>
2022-03-16 14:25:15 +0900

    [UPDATE]memo useMemo useCallBack でレンダリングを最適化

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import {useState} from "react";
+import {ChildArea} from "./ChildArea";
+import {useState, useCallback, useMemo} from "react";
 
 export const App = () => {
+
+    const [text, setText] = useState('')
+    const [open, setOpen] = useState(false)
     const [count, setCount] = useState(0)
 
-    const onClickCountUp = () => {
-        setCount(count + 1)
-    }
+    const onChangeText = (event) => setText(event.target.value)
+
+    const onClickOpen = () => setOpen(!open)
+
+    const onClickCountUp = () => setCount(count + 1)
+
+    const onClickClose = useCallback(
+        () => setOpen(false),
+        [setOpen])
+
+    // const temp = useMemo(() => 1 + 3,[])
+
     return (
         <>
-            <p>{count}</p>
+            <input type="text" value={text} onChange={onChangeText}/>
+            <br/>
+            <br/>
+            <button onClick={onClickOpen}>表示</button>
+            <ChildArea open={open} onClickClose={onClickClose}/>
             <button onClick={onClickCountUp}>カウントアップ</button>
+            <p>{count}</p>
         </>
     )
 }
